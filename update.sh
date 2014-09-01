6c98dc533ff19845f8ed9f49ea027625f0bde930
keenkim <solierkim94@gmail.com>
2014-09-01 22:28:33 +0900

    first commit test_node

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-echo `git add --all .`
+echo `git add --ignore-removal .`
 echo `git commit -m "first commit test_node"`
 echo `git remote add origin https://github.com/keenkim/secu.git`
 echo `git push -u origin master`
